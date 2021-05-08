1e73fbcfce3a3595be51b107012889091f43b8a5
Ju0632 <ju0632@outlook.com>
2021-05-08 18:23:43 +0800

    up

--- diy-part1.sh
+++ diy-part1.sh
@@ -12,6 +12,33 @@
 
 # Uncomment a feed source
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+#sed -i '$a src-git helloworld https://github.com/fw876/helloworld' feeds.conf.default
 
 # Add a feed source
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+
+#
+#                  佛祖保佑       永不宕机     永无BUG              //
+
+
+#                            _ooOoo_  
+#                           o8888888o  
+#                           88" . "88  
+#                           (| -_- |)  
+#                            O\ = /O  
+#                        ____/`---'\____  
+#                      .   ' \\| |// `.  
+#                       / \\||| : |||// \  
+#                     / _||||| -:- |||||- \  
+#                       | | \\\ - /// | |  
+#                     | \_| ''\---/'' | |  
+#                      \ .-\__ `-` ___/-. /  
+#                   ___`. .' /--.--\ `. . __  
+#                ."" '< `.___\_<|>_/___.' >'"".  
+#               | | : `- \`.;`\ _ /`;.`/ - ` : | |  
+#                 \ \ `-. \_ __\ /__ _/ .-` / /  
+#         ======`-.____`-.___\_____/___.-`____.-'======  
+#                            `=---='  
+#  
+#         .............................................  
+#                  佛祖保佑             永无BUG 
